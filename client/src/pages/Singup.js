4aaddf97f4ff40ded2051cda653d6311d7cb214a
Eddiem-11 <emonarrez.01@outllok.com>
2022-10-01 14:02:25 -0600

    added auth and singup js

--- client/src/pages/Singup.js
+++ client/src/pages/Singup.js
@@ -5,3 +5,37 @@ import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
 
 import Auth from "../utils/auth";
+import auth from "../utils/auth";
+
+const Signup = () => {
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [addUser, { error, data }] = useMutation(ADD_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...setFormState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+
+      auth.login(data.addUser.token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+};
