f06387faa65d5ae0f1231fec21e5d883bf5a0258
noellerjd <noellerninja@gmail.com>
2022-10-02 17:22:12 -0600

    added comments to models

--- server/models/User.js
+++ server/models/User.js
@@ -1,24 +1,28 @@
 const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
-
+// Model for users
 const userSchema = new Schema({
+  // Username format
   username: {
     type: String,
     required: true,
     unique: true,
     trim: true,
   },
+  // Email format
   email: {
     type: String,
     required: true,
     unique: true,
     match: [/.+@.+\..+/, "Must match an email address!"],
   },
+  // Password Format
   password: {
     type: String,
     required: true,
     minlength: 5,
   },
+  // Comment Format
   comments: [
     {
       type: Schema.Types.ObjectId,
@@ -26,7 +30,7 @@ const userSchema = new Schema({
     },
   ],
 });
-
+// if the user is recognised as new, creates the new user and encrypts the password.
 userSchema.pre("save", async function (next) {
   if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
@@ -35,11 +39,11 @@ userSchema.pre("save", async function (next) {
 
   next();
 });
-
+// check to see if the password is correct. compares password.
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
-
+// reference for User
 const User = model("User", userSchema);
-
+// export User
 module.exports = User;
