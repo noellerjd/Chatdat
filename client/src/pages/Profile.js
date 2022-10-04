87c678d22dfe0f94068e5056c80af283fc53c0ca
Eddiem-11 <emonarrez.01@outllok.com>
2022-10-03 21:19:40 -0600

    profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -12,4 +12,27 @@ const Profile = () => {
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
+
+  const user = data?.me || data?.user || {};
+
+  if (Auth.loggedIn() && Auth..getProfile().data.username === userParam) {
+    return <Navigate to="/me" />;
+  }
+
+  if (loading) {
+    return <div>Loading Profile please wait</div>
+  }
+
+  if (!user?.username) {
+    return (
+        <h2>
+            Please make sure you are logged in to see your profile...
+            use the Signup or the Signin forms to continue!
+        </h2>
+    );
+  }
+
+//   Here we are going to create the front end element of how the profile page will load and look
 };
+
+export default Profile;
