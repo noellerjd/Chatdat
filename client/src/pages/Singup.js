8823aa059d0b2a8f448732a60c9c364954988df1
Eddiem-11 <emonarrez.01@outllok.com>
2022-10-03 18:30:20 -0600

    added html to signup.js

--- client/src/pages/Singup.js
+++ client/src/pages/Singup.js
@@ -38,4 +38,55 @@ const Signup = () => {
       console.log(e);
     }
   };
+
+  return (
+    <main className="flex-row justify-center ">
+      <div>
+        <div className="card">
+          <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
+          <div className="card-body">
+            {data ? (
+              <p></p>
+            ) : (
+              <form onSubmit={handleFormSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="USERNAME"
+                  name="username"
+                  type="text"
+                  value={formState.name}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="EMAIL"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="********"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+                <button className="btn btn-block btn-primary" type="submit">
+                  Submit
+                </button>
+              </form>
+            )}
+
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
