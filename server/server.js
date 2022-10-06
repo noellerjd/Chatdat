bf7b08f55a0286e8971211d676c4c5e59e7fec4a
noellerjd <noellerninja@gmail.com>
2022-10-05 19:44:02 -0600

    remove pusher

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
@@ -15,15 +15,15 @@ const server = new ApolloServer({
 });
 
 const cors = require("cors");
-const Pusher = require("pusher");
+// const Pusher = require("pusher");
 
-const pusher = new Pusher({
-  appId: "1487565",
-  key: "58a90caa8d56aedfdfb0",
-  secret: "32b695b8cc64ae8fd488",
-  cluster: "us3",
-  useTLS: true,
-});
+// const pusher = new Pusher({
+//   appId: "1487565",
+//   key: "58a90caa8d56aedfdfb0",
+//   secret: "32b695b8cc64ae8fd488",
+//   cluster: "us3",
+//   useTLS: true,
+// });
 
 app.use(
   cors({
