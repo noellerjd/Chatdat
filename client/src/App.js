92746c03ef49264e436ac3f9a19a183a18662010
Quy Nguyen <qnguyen303@gmail.com>
2022-10-05 19:10:59 -0600

    fixing signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,42 @@
 import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 import Headers from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import { setContext } from "@apollo/client/link/context";
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <Headers />
-      <Main />
-      <Footer />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Headers />
+        <Main />
+        <Footer />
+      </div>
+    </ApolloProvider>
   );
 }
 
