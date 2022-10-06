018b417374739f03b025863bd7e0494a1693d45e
noellerjd <noellerninja@gmail.com>
2022-10-05 19:44:40 -0600

    delete pusher

--- server/server.js
+++ server/server.js
@@ -14,38 +14,6 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-const cors = require("cors");
-// const Pusher = require("pusher");
-
-// const pusher = new Pusher({
-//   appId: "1487565",
-//   key: "58a90caa8d56aedfdfb0",
-//   secret: "32b695b8cc64ae8fd488",
-//   cluster: "us3",
-//   useTLS: true,
-// });
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:8080",
-      "http://localhost:4200",
-    ],
-  })
-);
-
-app.use(express.json());
-
-app.post("/api/messages", async (req, res) => {
-  await pusher.trigger("chat", "message", {
-    username: req.body.username,
-    message: req.body.message,
-  });
-
-  res.json([]);
-});
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
