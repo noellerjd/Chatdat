bcaa6733fd4f90ce18fe780afd0a8f993634f084
noellerjd <noellerninja@gmail.com>
2022-10-07 20:08:52 -0600

    profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -15,24 +15,24 @@ const Profile = () => {
 
   const user = data?.me || data?.user || {};
 
-  if (Auth.loggedIn() && Auth..getProfile().data.username === userParam) {
+  if (Auth.loggedIn() && Auth.getProfile().data.username === useParam) {
     return <Navigate to="/me" />;
   }
 
   if (loading) {
-    return <div>Loading Profile please wait</div>
+    return <div>Loading Profile please wait</div>;
   }
 
   if (!user?.username) {
     return (
-        <h2>
-            Please make sure you are logged in to see your profile...
-            use the Signup or the Signin forms to continue!
-        </h2>
+      <h2>
+        Please make sure you are logged in to see your profile... use the Signup
+        or the Signin forms to continue!
+      </h2>
     );
   }
 
-//   Here we are going to create the front end element of how the profile page will load and look
+  //   Here we are going to create the front end element of how the profile page will load and look
 };
 
 export default Profile;
