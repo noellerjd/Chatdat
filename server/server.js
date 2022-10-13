987d2acef01b1728f6886034fd6b3822fe5c710a
noellerjd <noellerninja@gmail.com>
2022-10-12 19:27:25 -0600

    heroku fix

--- server/server.js
+++ server/server.js
@@ -32,9 +32,9 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
